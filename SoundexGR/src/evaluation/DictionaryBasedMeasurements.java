d8b00b84bd94ce4bc9fda74c3200b5e1d0417726
YannisTzitzikas <tzitzik@ics.forth.gr>
2020-08-09 21:01:45 +0300

    Addition of an inverted index-like structure for analyzing the distribution of soundexGR codes and stems.

--- SoundexGR/src/evaluation/DictionaryBasedMeasurements.java
+++ SoundexGR/src/evaluation/DictionaryBasedMeasurements.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -24,7 +25,7 @@ import stemmerWrapper.StemmerWrapper;
 public class DictionaryBasedMeasurements {
 	
 	
-	/** Prints the contents of a map String-Integer
+	/** Prints the contents of a map String->Integer
 	 * 
 	 * @param map
 	 */
@@ -37,6 +38,26 @@ public class DictionaryBasedMeasurements {
 		
 	}
 	
+	/**
+	 * It prints an inverted index (a map of words to sets of words)
+	 * @param ii : the map
+	 * @param verbose  : if false only the size of the posting list of each term is shown, if true all elements are shown too
+	 */
+	private static void printInvertedIndex(Map<String, HashSet<String>> ii, boolean verbose) {
+	    for (String code:  ii.keySet()) {
+	    	System.out.printf("%s,%d\n", code, ii.get(code).size());
+	    	if (verbose) {
+	    		for (String w: ii.get(code)) {
+	    			System.out.println("\t"+w);
+	    		}
+	    	}
+	    }
+	}
+	
+	/**
+	 * It performs a few measurements over a dictionary (about the distribution of codes)
+	 * @param path  the file path of the dictionary
+	 */
 	public static void performMeasurements(String path) {
 			int counter = 0;  // word counter
 			int totalCharNum = 0;
@@ -44,12 +65,19 @@ public class DictionaryBasedMeasurements {
 			int wordMaxSize = 0;
 			int curWordSize = 0;
 	        String line;
+	        int codesCharSize = 0;
 	        long startTime=0, endTime=0, totalTime = 0;
-	        String wordInSoundex; 
+	        String wordEncoded=""; 
 	        
-	        //extra for codes' analytics
+	        int option2test = 1 ; // 1: CoundexGRExtra, 2: stemmer
+	        
+	        
+	        //extra for codes' analytics (it measures the number of words assigned the same code)
 	        Map<String, Integer> codesAndCounts = new HashMap<>(); // for code analytics
-	        StemmerWrapper stemmer= new StemmerWrapper (); // for the case we want to perform measurement using a stemmer	        
+	        StemmerWrapper stemmer= new StemmerWrapper (); // for the case we want to perform analogous measurements using a stemmer	        
+	        
+	        // DETAILED for INSPECTION
+	        Map<String, HashSet<String>> codesAndWords = new HashMap<>(); // for code analytics     
 	        
 	        
 			try {
@@ -63,15 +91,30 @@ public class DictionaryBasedMeasurements {
 		        	if (curWordSize <wordMinSize ) wordMinSize=curWordSize;  // for min/max word sizes
 		        	if (curWordSize >wordMaxSize ) wordMaxSize =curWordSize;
 		        	
-		        	wordInSoundex = SoundexGRExtra.encode(line); // for testing SoundexGRExtra
-		        	//wordInSoundex = stemmer.getStemOf(line); 		// for testing a stemmer
-		       
+		        	if (option2test==1) {
+		        		wordEncoded = SoundexGRExtra.encode(line); // for testing SoundexGRExtra
+		        	} else if  	(option2test==2) {
+		        		wordEncoded = stemmer.getStemOf(line); 		// for testing a stemmer
+		        	} 
+		        	
+		        	
+		        	codesCharSize+=wordEncoded.length();
 		        	
 		        	//System.out.printf("%10s --> %s\n", line, wordInSoundex); // for debugging only
 		        	
 		        	//extra for analytics (counting how many words go to the same code/stem
-		        	 Integer codefreq = codesAndCounts.get(wordInSoundex);
-		        	 codesAndCounts.put(wordInSoundex, (codefreq == null) ? 1 : codefreq + 1);
+		        	 Integer codefreq = codesAndCounts.get(wordEncoded);
+		        	 codesAndCounts.put(wordEncoded, (codefreq == null) ? 1 : codefreq + 1);
+		        	 
+		        	 //DETAILED ANALYSIS
+		        	 HashSet wordsWithThatCode = codesAndWords.get(wordEncoded);
+		        	 if (wordsWithThatCode==null) { // the code is not in the map
+		        		 wordsWithThatCode = new HashSet();
+		        		 wordsWithThatCode.add(line);
+		        		 codesAndWords.put(wordEncoded,wordsWithThatCode);
+		        	 } else {
+		        	 wordsWithThatCode.add(line);
+		        	 }
 		        }
 	        } catch (Exception e) {
 	        	System.out.println(e);
@@ -80,6 +123,8 @@ public class DictionaryBasedMeasurements {
 	        endTime = System.nanoTime();
 	        totalTime = endTime - startTime;
 
+	        
+	        System.out.println("\tTEST OPTION      : " +  ( (option2test==1) ? "SoundexGRExtra": "Stemmer"));
 	        	        
 	        System.out.println("\tElapsed time     : " + TimeUnit.SECONDS.convert(totalTime, TimeUnit.NANOSECONDS)+ " secs");
 	        System.out.println("\tElapsed time     : " + TimeUnit.MILLISECONDS.convert(totalTime, TimeUnit.NANOSECONDS)+ " msecs");
@@ -88,6 +133,7 @@ public class DictionaryBasedMeasurements {
 	        System.out.println("Avg word size             : " + (totalCharNum+0.0)/counter);
 	        System.out.println("Min word size             : " + wordMinSize);
 	        System.out.println("Max word size             : " + wordMaxSize);
+	        System.out.println("Average code size         : " + (codesCharSize+0.0)/counter);
 	        
 	        // Analytics for the codes
 	        System.out.println("Number of Distinct codes  : " + codesAndCounts.keySet().size());
@@ -105,6 +151,9 @@ public class DictionaryBasedMeasurements {
 	        System.out.println("Min number of words of a code: " + minCount);
 	        System.out.println("Max number of words of a code: " + maxCount);
 	        //printMap(codesAndCounts);
+	        
+	        //Detailed
+	        printInvertedIndex(codesAndWords,false);
 	}
 	
 	public static void main(String[] args) {
